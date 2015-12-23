a4ff5a4f1079fd142aa8e2710b3cd4958ea77560
dusthoff <dustinh@onesi.de>
2015-12-23 14:33:54 +0100

    Search function to find another city

---
+++ src/main/java/gadget/component/api/CitySearch.java
@@ -0,0 +1,27 @@
+package gadget.component.api;
+
+import gadget.component.job.owm.OWM;
+import gadget.component.job.owm.data.City;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by Dustin on 23.12.2015.
+ */
+public class CitySearch extends ApiComponent<String> {
+    @Override
+    public Object handleRequest(String request, String requestMethod) throws Exception {
+        if(request==null)request="";
+        if (OWM.getInstance() == null) return Collections.emptyList();
+        List<City> citylist = OWM.getInstance().getCities();
+        List<City> result = new ArrayList<City>();
+        if (request.length() > 4) {
+            for (City city : citylist) {
+                if (city.getName().matches(request + ".*")) result.add(city);
+            }
+        } else result = citylist.subList(0, 40);
+        return result;
+    }
+}
---
+++ src/test/java/gadget/component/api/CitySearchTest.java
@@ -0,0 +1,32 @@
+package gadget.component.api;
+
+import gadget.component.job.owm.data.City;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
+/**
+ * Created by Dustin on 23.12.2015.
+ */
+public class CitySearchTest extends ApiRegistryTest {
+
+    @Test
+    public void simpleSearch() throws Throwable {
+        List<City> result = client.searchCity(null);
+        Assert.assertNotNull(result);
+        Assert.assertEquals(40,result.size());
+    }
+    @Test
+    public void searchOsnabrueck() throws Throwable {
+        List<City> result = client.searchCity("Osnab");
+        Assert.assertNotNull(result);
+
+    }
+
+    @Test
+    public void searchBerlin() throws Throwable {
+        List<City> result = client.searchCity("Berli");
+        Assert.assertNotNull(result);
+    }
+}
